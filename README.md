1d752d1fc492ab89ede96b2b0a0f4cfad578bdf9
XER0AAR0N <acdelectrical@icloud.com>
2026-01-16 22:35:52 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+﻿# Digg Desktop WebView2 Client (Unofficial)
+
+An **unofficial desktop client** for the news and social platform **Digg**.
+
+> ⚠️ This project is **not affiliated with, endorsed by, or sponsored by Digg**.
+
+---
+
+## 📸 Screenshot
+
+![Digg Desktop Client Screenshot](digg-client-screenshot.png)
+
+---
+
+## ✨ Features
+
+- Fast navigation without the browser clutter
+- Somewhat light-weight built in RUST using WebView2
+
+(PLEASE READ)
+Please note,this project kinda is as-is and I'm unsure wether to make this anything more than a "WebView"
+for the Digg site, this is mainly due to being unaware on the Digg teams stance on third party clients, hence this
+being a WebView2 application, loading the Digg website using a client spoofer. (seems Digg blocks projects like this
+by default, as the site doesn't load without spoofing)
---
+++ src/main.rs
@@ -0,0 +1,57 @@
+// Digg for Windows created by Aaron Doe (XER0AAR0N)
+//
+// This is an unofficial client and is not affiliated with
+// the official Digg development team in ANY way
+
+#![windows_subsystem = "windows"]
+
+use tao::{
+    dpi::LogicalSize,
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::{Icon, WindowBuilder},
+};
+
+use wry::WebViewBuilder;
+
+fn load_icon() -> Icon {
+    let buffer = include_bytes!("digg.ico");
+    let image = image::load_from_memory(buffer)
+        .expect("Failed to load icon")
+        .to_rgba8();
+    let (width, height) = image.dimensions();
+
+    Icon::from_rgba(image.into_raw(), width, height)
+        .expect("Failed to create window icon")
+}
+
+fn main() {
+    let event_loop = EventLoop::new();
+
+    let window = WindowBuilder::new()
+        .with_title("Digg")
+        .with_inner_size(LogicalSize::new(1200.0, 1024.0))
+        .with_window_icon(Some(load_icon()))
+        .build(&event_loop)
+        .expect("Failed to create window");
+
+// Client spoofer is needed in order for Digg site not to throw an error whilst loading
+
+    let _webview = WebViewBuilder::new(&window)
+        .with_user_agent(
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+             AppleWebKit/537.36 (KHTML, like Gecko) \
+             Chrome/120.0.0.0 Safari/537.36",
+        )
+        .with_url("https://digg.com")
+        .build()
+        .expect("Failed to create WebView");
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+
+        if let Event::WindowEvent { event: WindowEvent::CloseRequested, .. } = event {
+            *control_flow = ControlFlow::Exit;
+        }
+    });
+}
